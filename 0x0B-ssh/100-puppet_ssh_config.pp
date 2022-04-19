fe92842ccd33ff12c0b46cae295a6bfb98e8e286
juan-gimenez <juancitogimenez0@gmail.com>
2022-04-19 11:50:20 -0700

    change config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# change config file using puppet
+# using file_line (.pp)
+
+file_line {'Turn off passwd auth':
+   path => '/etc/ssh/ssh_config',
+   line => 'PasswordAuthentication no',
+}
+
+file_line {'Declare identity file':
+   path => '/etc/ssh/ssh_config',
+   line => 'IdentityFile ~/.ssh/school',
+}
