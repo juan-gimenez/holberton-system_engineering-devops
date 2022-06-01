4a66b4006770b5136045a5c6f27b8e2c75da90b6
juan-gimenez <juancitogimenez0@gmail.com>
2022-06-01 10:36:58 -0700

    puppet lint fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 # Apache is returning a 500 error
-exec { ' debug ':
-       onlyif  => 'test -e /var/www/html/wp-settings.php',
-       command => "sudo sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-       path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-=======
-
 exec { ' debug ':
   onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sudo sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
->>>>>>> f4a3bb878eaf0a3914205709df41e84f59624ad7
