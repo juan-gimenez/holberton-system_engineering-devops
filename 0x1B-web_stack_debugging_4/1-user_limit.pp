8102936542d4875f5346a67d195118a2b66d38a6
juan-gimenez <juancitogimenez0@gmail.com>
2022-06-09 15:49:15 -0700

    task adv

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# coment
+exec { 'h':
+  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
+}
+
+exec { 's':
+  command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
+}
